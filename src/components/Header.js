256903d77318ee3f7fd6a9d6181063a854b0caca
Yafira <yafira.martinez@lc.cuny.edu>
2022-04-01 23:53:15 -0400

    added missing alt props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 export default function Header() {
 	return (
 		<header className='header'>
-			<img src='./images/troll-face.png' className='header--image' />
+			<img
+				src='./images/troll-face.png'
+				className='header--image'
+				alt='troll face'
+			/>
 			<h2 className='header--title'>Meme Generator</h2>
 			<h4 className='header--project'>Online image maker</h4>
 		</header>
-	);
+	)
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 export default function Meme() {
 	const [meme, setMeme] = React.useState({
@@ -6,35 +6,35 @@ export default function Meme() {
 		bottomText: '',
 		randomImage:
 			'https://ichef.bbci.co.uk/news/976/cpsprodpb/F1F2/production/_118283916_b19c5a1f-162b-410b-8169-f58f0d153752.jpg',
-	});
-	const [allMemes, setAllMemes] = React.useState([]);
+	})
+	const [allMemes, setAllMemes] = React.useState([])
 
 	React.useEffect(() => {
 		async function getMemes() {
-			const res = await fetch('https://api.imgflip.com/get_memes');
-			const data = await res.json();
-			setAllMemes(data.data.memes);
+			const res = await fetch('https://api.imgflip.com/get_memes')
+			const data = await res.json()
+			setAllMemes(data.data.memes)
 		}
-		getMemes();
-	}, []);
+		getMemes()
+	}, [])
 
 	// console.log(allMemes);
 
 	function getMemeImage() {
-		const randomNumber = Math.floor(Math.random() * allMemes.length);
-		const url = allMemes[randomNumber].url;
+		const randomNumber = Math.floor(Math.random() * allMemes.length)
+		const url = allMemes[randomNumber].url
 		setMeme((prevMeme) => ({
 			...prevMeme,
 			randomImage: url,
-		}));
+		}))
 	}
 
 	function handleChange(event) {
-		const { name, value } = event.target;
+		const { name, value } = event.target
 		setMeme((prevMeme) => ({
 			...prevMeme,
 			[name]: value,
-		}));
+		}))
 	}
 
 	return (
@@ -61,10 +61,10 @@ export default function Meme() {
 				</button>
 			</div>
 			<div className='meme'>
-				<img src={meme.randomImage} className='meme--image' />
+				<img src={meme.randomImage} className='meme--image' alt='random meme' />
 				<h2 className='meme--text top'>{meme.topText}</h2>
 				<h2 className='meme--text bottom'>{meme.bottomText}</h2>
 			</div>
 		</main>
-	);
+	)
 }
